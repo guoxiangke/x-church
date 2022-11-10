e69ab5c9338d450d18f9d92e59b3c3880604fae4
Dale <xiangkeguo@gmail.com>
2022-11-09 21:47:26 -0800

    nova no1

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -41,6 +41,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewNova', function ($user) {
+            return $user->id ===1;
             return in_array($user->email, [
                 //
             ]);
