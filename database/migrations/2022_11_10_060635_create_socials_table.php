43d78860a137004cf955d7d34b5db5d204085231
Dale <xiangkeguo@gmail.com>
2022-11-09 21:30:08 -0800

    add table socials

---
+++ database/migrations/2022_11_10_060635_create_socials_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('socials', function (Blueprint $table) {
+            $table->id();
+            $table->string('social_id')->index();//->comment('openid');
+            $table->unsignedBigInteger('user_id');
+            $table->string('name')->nullable();
+            $table->string('avatar')->nullable();
+            $table->unsignedTinyInteger('type')->default(0); //0:wechat 1:github 2:facebook 3:psid
+            $table->timestamps();
+
+            $table->foreign('user_id')
+               ->references('id')
+               ->on('users')
+               ->onDelete('cascade');
+            // add 唯一索引在 social_id + type //确保一个用户在一个平台唯一绑定
+            $table->unique(['social_id', 'type']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('socials');
+    }
+};
