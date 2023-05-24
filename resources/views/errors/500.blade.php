c316304e1f05e83b207b353de88b13e00822d0ac
Dale <xiangkeguo@gmail.com>
2023-05-24 14:32:15 -0700

    add 500 pointer up

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,12 +1,16 @@
-@section('title', '请点击右下角 使用完整服务')
+@section('title', '授权访问，使用完整服务')
 
 <x-wechat-layout>
     <div class="page">
         <div class="page__bd page__bd_spacing">
+            <br/><br/>
+            <p class="page__desc"  style="text-align: center;">要使用完整服务，请授权访问您的头像和昵称</p>
             <br/>
-            <a href="javascript:" role="button" title="等待中" class="weui-btn weui-btn_primary weui-btn_loading"><span class="weui-primary-loading weui-primary-loading_transparent"><i class="weui-primary-loading__dot"></i></span> 请点击右下角授权访问您的昵称</a>
+            <a href="javascript:" role="button" title="等待中" class="weui-btn weui-btn_default">请点右下角↘</a>
+            
+            
 
-            <img  src="{{ asset('/images/pointer.webp') }}" style="transform: rotate(90deg); position: fixed; bottom: 0;right: 0;">
+            <img  src="{{ asset('/images/pointer.webp') }}" style="transform: rotate(90deg); position: fixed; bottom: 60px;right: 0;">
         </div>
     </div>
 </x-wechat-layout>
