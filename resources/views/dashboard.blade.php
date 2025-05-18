3645d2df60986c724a8a7a24bbc9b1aae8a7f8c0
me@mbp.m3 <TWR-****-****-****-7F47>
2025-05-17 20:39:12 -0700

    要使用完整服务

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,9 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                   要使用完整服务，请授权访问您的头像和昵称
-                </h2>
+                <x-jet-welcome />
             </div>
         </div>
     </div>
